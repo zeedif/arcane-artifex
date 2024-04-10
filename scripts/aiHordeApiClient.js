ea39111c1f622467bbaf27266eb22710207137d8
John Pollock <pollockjj@gmail.com>
2024-04-10 17:37:21 -0500

    Refactor AI Horde API client class in aiHordeApiClient.js

--- scripts/aiHordeApiClient.js
+++ scripts/aiHordeApiClient.js
@@ -1,35 +1,32 @@
 class HordeAPIClient {
-    async checkStatus() {
-      // Retrieve the selected source from the game settings
-      const selectedSource = game.settings.get('stable-images', 'source');
+  async checkStatus() {
+    const selectedSource = game.settings.get('stable-images', 'source');
   
-      // Only proceed if the stableHorde option is selected
-      if (selectedSource === 'stableHorde') {
-        const aiHordeUrl = game.settings.get('stable-images', 'horde_url');
-        const statusUrl = `${aiHordeUrl}/api/v2/status/heartbeat`;
+    if (selectedSource === 'stableHorde') {
+      const aiHordeUrl = game.settings.get('stable-images', 'horde_url');
+      const statusUrl = `${aiHordeUrl}/api/v2/status/heartbeat`;
         
-        try {
-          const response = await fetch(statusUrl);
-          if (response.ok) {
-            console.log('AI Horde server is accessible at:', aiHordeUrl);
-            ui.notifications.info('AI Horde server is accessible.');
-            return 'AI Horde API is accessible.';
-          } else {
-            console.error('AI Horde server is not accessible: response code', response.status, 'at URL:', aiHordeUrl);
-            ui.notifications.error(`AI Horde server is not accessible: response code: ${response.status}`);
-            throw new Error(`AI Horde API returned an error: ${response.status}`);
-          }
-        } catch (error) {
-            console.error('Error occurred while trying to access AI Horde server at URL:', aiHordeUrl, '; error =', error);
-            ui.notifications.error(`Error occurred while trying to access AI Horde server; error = ${error}`);
+      try {
+        const response = await fetch(statusUrl);
+        if (response.ok) {
+          console.log('AI Horde server is accessible at:', aiHordeUrl);
+          ui.notifications.info('AI Horde server is accessible.');
+          return 'AI Horde API is accessible.';
+        } else {
+          console.error('AI Horde server is not accessible: response code', response.status, 'at URL:', aiHordeUrl);
+          ui.notifications.error(`AI Horde server is not accessible: response code: ${response.status}`);
+          throw new Error(`AI Horde API returned an error: ${response.status}`);
         }
-      } else {
-        console.log("Stable Horde is not selected. Skipping AI Horde status check.");
-        // Optionally, you could return a message or handle the skipped check appropriately
-        return 'Stable Horde is not selected. Skipping AI Horde status check.';
+      } catch (error) {
+        console.error('Error occurred while trying to access AI Horde server at URL:', aiHordeUrl, '; error =', error);
+        ui.notifications.error(`Error occurred while trying to access AI Horde server; error = ${error}`);
       }
+    } else {
+      console.log("Stable Horde is not selected. Skipping AI Horde status check.");
+      return 'Stable Horde is not selected. Skipping AI Horde status check.';
     }
   }
-  
-  export const aiHordeApiClient = new HordeAPIClient();
-  export default aiHordeApiClient;
+}
+
+export const aiHordeApiClient = new HordeAPIClient();
+export default aiHordeApiClient;
--- scripts/aiHordeSettings.js
+++ scripts/aiHordeSettings.js
@@ -9,67 +9,58 @@ export default class HordeSettings extends FormApplication {
             template: 'modules/stable-images/templates/aihorde-settings.hbs',
             width: 700,
             height: "auto",
-            rezisable: true
+            resizable: true
         });
     }
 
-    /**
-     * Retrieve data for the form.
-     * @returns {Promise<Object>} The form data.
-     */
     async getData() {
         const savedSettings = game.settings.get('stable-images', 'stable-settings') || {};
         console.error("savedSettings:", savedSettings);
-      
+
         const { horde_models, horde_model } = await this.loadHordeModels();
         const samplers = await this.loadHordeSamplers();
-      
-        // Merge defaults with saved settings, with saved settings taking precedence
+
         const context = mergeObject(defaultSettings, savedSettings);
         context.horde_models = horde_models;
         context.horde_model = horde_model;
-        context.horde_sampler = savedSettings.horde_sampler || 'k_euler'; // Default to 'k_euler' if not set
+        context.horde_sampler = savedSettings.horde_sampler || 'k_euler';
         context.samplers = await this.loadHordeSamplers();
         context.source = game.settings.get("stable-images", "source");
-      
-      
+
         this.context = context;
         return context;
-      }
-
+    }
 
     async loadHordeModels() {
         try {
             const hordeUrl = game.settings.get('stable-images', 'horde_url');
             const response = await fetch(`${hordeUrl}/api/v2/status/models`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             });
-        
+
             if (response.ok) {
-            const data = await response.json();
-            data.sort((a, b) => b.count - a.count);
-        
-            const horde_models = data.map(x => ({
-                value: x.name,
-                text: `${x.name} (ETA: ${x.eta}s, Queue: ${x.queued}, Workers: ${x.count})`
-            }));
-        
-            // Retrieve the horde_model from the settings
-            const savedSettings = game.settings.get('stable-images', 'stable-settings') || {};
-            let horde_model = savedSettings.horde_model;
-        
-            // If the horde_model is not set or is not in the horde_models array, set it to the first model
-            if (!horde_model || !horde_models.some(model => model.value === horde_model)) {
-                horde_model = horde_models[0].value;
-            }
-        
-            return { horde_models, horde_model };
+                const data = await response.json();
+                data.sort((a, b) => b.count - a.count);
+
+                const horde_models = data.map(x => ({
+                    value: x.name,
+                    text: `${x.name} (ETA: ${x.eta}s, Queue: ${x.queued}, Workers: ${x.count})`
+                }));
+
+                const savedSettings = game.settings.get('stable-images', 'stable-settings') || {};
+                let horde_model = savedSettings.horde_model;
+
+                if (!horde_model || !horde_models.some(model => model.value === horde_model)) {
+                    horde_model = horde_models[0].value;
+                }
+
+                return { horde_models, horde_model };
             } else {
-            ui.notifications.error(`Error while retrieving Horde models: ${response.statusText}`);
-            return { horde_models: [], horde_model: '' };
+                ui.notifications.error(`Error while retrieving Horde models: ${response.statusText}`);
+                return { horde_models: [], horde_model: '' };
             }
         } catch (error) {
             ui.notifications.error(`Error while retrieving Horde models: ${error}`);
@@ -77,63 +68,42 @@ export default class HordeSettings extends FormApplication {
         }
     }
 
-
-    /**
-     * Load Horde samplers.
-     * @returns {Promise<Array>} An array of sampler objects.
-     */
     async loadHordeSamplers() {
         return [
-          { name: 'k_lms' },
-          { name: 'k_heun' },
-          { name: 'k_euler' },
-          { name: 'k_dpm_2' },
-          { name: 'k_dpm_2_a' },
-          { name: 'DDIM' },
-          { name: 'PLMS' },
-          { name: 'k_dpm_fast' },
-          { name: 'k_dpm_adaptive' },
-          { name: 'k_dpmpp_2s_a' },
-          { name: 'k_dpmpp_2m' },
-          { name: 'dpmsolver' }
+            { name: 'k_lms' },
+            { name: 'k_heun' },
+            { name: 'k_euler' },
+            { name: 'k_dpm_2' },
+            { name: 'k_dpm_2_a' },
+            { name: 'DDIM' },
+            { name: 'PLMS' },
+            { name: 'k_dpm_fast' },
+            { name: 'k_dpm_adaptive' },
+            { name: 'k_dpmpp_2s_a' },
+            { name: 'k_dpmpp_2m' },
+            { name: 'dpmsolver' }
         ];
-      }
-    /**
-     * Activate form listeners.
-     * @param {JQuery} html - The form HTML.
-     */
+    }
+
     activateListeners(html) {
         super.activateListeners(html);
         html.find('[name="horde_nsfw"]').change(event => this.onToggleChange(event));
-      
-        // Add event listener for source selection change
+
         html.find('select[name="source"]').on("change", async (event) => {
-          await game.settings.set("stable-images", "source", event.target.value);
-          this.render();
+            await game.settings.set("stable-images", "source", event.target.value);
+            this.render();
         });
-      }
+    }
 
-
-
-    /**
-     * Handle toggle change event.
-     * @param {Event} event - The toggle change event.
-     */
     async onToggleChange(event) {
         // Handle toggle change event
     }
 
-    /**
-     * Update form object.
-     * @param {Event} event - The form submission event.
-     * @param {Object} formData - The form data.
-     */
     async _updateObject(event, formData) {
         const savedSettings = game.settings.get('stable-images', 'stable-settings');
         const updatedSettings = mergeObject(savedSettings, formData);
         await game.settings.set('stable-images', 'stable-settings', updatedSettings);
-      
-        // Re-render the form with the updated data
+
         this.render(true);
-      }
+    }
 }
